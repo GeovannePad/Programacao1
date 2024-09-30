543f02107b120a53cdb7cab73d4b2f5e563cc4c5
Giovanni <geovannepadilha@hotmail.com>
2024-09-30 19:07:41 -0300

    Aula prática 2 da disciplina Programação I - Arrays, Generics, Lists, IEnumerables, Dictionaries and Extension Methods

---
+++ Aula2/PraticaComArrays.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Aula2
+{
+    public class PraticaComArrays
+    {
+        public void Exercicio1()
+        {
+            int[] arrayIntLinear = new int[100000];
+
+            // Popular as posições com seu valor de índice linearmente
+            for (int i = 0; i < 100000; i++)
+            {
+                arrayIntLinear[i] = i;
+            }
+
+            int[,] matriz = new int[100, 100];
+            var rand = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                for (int k = 0; k < 100; k++)
+                {
+                    matriz[i, k] = rand.Next(1, 300000);
+                }
+            }
+
+            int countMatch = 0;
+            for (int i = 0; i < matriz.GetUpperBound(0); i++)
+            {
+                for (int k = 0; k  < matriz.GetUpperBound(1); k ++)
+                {
+                    var valorMatriz = (int)matriz.GetValue(i, k);
+
+                    for (int x = 0; x < arrayIntLinear.Length; x++)
+                    {
+                        if (arrayIntLinear[x] == valorMatriz)
+                        {
+                            countMatch++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Houveram {countMatch} entre os valores randômicos da matriz e do array linear!");
+        }
+
+        public void Exercicio2()
+        {
+            Console.WriteLine("*******************************************************");
+
+            // Declarando arrays de base para o exercício
+            char[] alfabetoArray = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            char[] arrayCharAleatorio = new char[100];
+            char[] arrayFinal = new char[100];
+            Random rand = new Random();
+
+            // Populando o array aleatório
+            for (int i = 0; i < 100; i++)
+            {
+                arrayCharAleatorio[i] = alfabetoArray[rand.Next(0, alfabetoArray.Length)];
+            }
+
+            int posInicial = 0;
+            int posFinal = 0;
+
+            Console.WriteLine("Informa um valor inicial para recortar do array original:");
+            posInicial = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informa um valor final (maior que valor anterior) para recortar do array original:");
+            posFinal = int.Parse(Console.ReadLine());
+
+            for (int i = posInicial; i < posFinal; i++)
+            {
+                arrayFinal[i] = arrayCharAleatorio[i];
+            }
+
+            var stringFinal = new string(arrayFinal);
+
+            Console.WriteLine("A string final ficou assim:");
+            Console.WriteLine(stringFinal);
+        }
+    }
+}
---
+++ Aula2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp.Aula2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Aula Prática de C# 2!");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Arrays");
+
+            PraticaComArrays praticaComArrays = new PraticaComArrays();
+            praticaComArrays.Exercicio1();
+            praticaComArrays.Exercicio2();
+
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Generics");
+
+            PraticaComFilasBoxing praticaComFilasBoxing = new PraticaComFilasBoxing();
+            praticaComFilasBoxing.ExercicioBoxingFilas();
+
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Listas, IEnumerables e Dicionários");
+
+            PraticaManipulandoCollections praticaManipulandoCollections = new PraticaManipulandoCollections();
+            praticaManipulandoCollections.Exercicio1();
+
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Métodos de extensão");
+
+            PraticaExtensionMethods praticaExtensionMethods = new PraticaExtensionMethods();
+            praticaExtensionMethods.Exercicio1();
+        }
+    }
+}
